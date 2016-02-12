d1772a533d3e9b84c9cc7fcb8521fe58fd509e62
Bluthman2 <bluthman2@gmail.com>
2016-02-12 18:53:43 -0500

    no color

--- ic2_lcd.js
+++ ic2_lcd.js
@@ -2,7 +2,7 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(1, 0x20);
  
-lcd.backlight(lcd.colors.RED);
+// lcd.backlight(lcd.colors.RED);
 lcd.message('Hello World!');
  
 lcd.on('button_change', function(button) {
