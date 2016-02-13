a885d1b6f4427b24072de6d52505f1d73e77e5f5
Bluthman2 <bluthman2@gmail.com>
2016-02-12 20:06:55 -0500

    test

--- ic2_lcd.js
+++ ic2_lcd.js
@@ -1,9 +1,10 @@
 var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
-lcd = new LCDPLATE(1, 0x27);
+lcd = new LCDPLATE(1, 0x27,-1);
  
 // lcd.backlight(lcd.colors.RED);
 console.log("Working on lcd!");
 lcd.clear();
 
 lcd.message("Hello world!");
+ lcd.sendBytes(0, 0x3F); // Sainsmart 1602 I2C backlight off
