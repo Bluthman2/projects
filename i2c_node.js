e9501dec7f0a2c507af437e98af5f14904ce0223
Bluthman2 <bluthman2@gmail.com>
2016-02-12 19:42:34 -0500

    fix

--- i2c_node.js
+++ i2c_node.js
@@ -3,6 +3,7 @@ var address = 0x27;
 var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface 
  
 console.log("Writing to lcd.");
+
 wire.scan(function(err, data) {
   // result contains an array of addresses 
 });
@@ -12,6 +13,7 @@ wire.writeByte(byte, function(err) {});
 wire.writeBytes(command, [byte0, byte1], function(err) {});
  
 wire.readByte(function(err, res) { // result is single byte }) 
+});
  
 wire.readBytes(command, length, function(err, res) {
   // result contains a buffer of bytes 
