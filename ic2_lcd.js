5726f258bf7ecdeb76e073fa3fe3085ad8b9e849
Bluthman2 <bluthman2@gmail.com>
2016-02-12 19:16:13 -0500

    test

--- ic2_lcd.js
+++ ic2_lcd.js
@@ -3,6 +3,7 @@ LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(1, 0x27);
  
 // lcd.backlight(lcd.colors.RED);
+console.log("Working on lcd!");
 lcd.message('Hello World!');
  
 lcd.on('button_change', function(button) {
