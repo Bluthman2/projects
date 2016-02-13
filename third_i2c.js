058c4e30803537236011163eebccdc97a5a01c0f
Bluthman2 <bluthman2@gmail.com>
2016-02-12 20:01:48 -0500

    test

--- third_i2c.js
+++ third_i2c.js
@@ -8,4 +8,12 @@ console.log(result);
 }
 });
 
+rasp2c.set(‘0xa1′, ‘0x27′, ‘0xff’, function(err, result) {
+if (err) {
+console.log(err);
+} else {
+console.log(result);
+}
+});
+
 console.log("detect");
