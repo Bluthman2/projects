471b5ff35ed22ce0de8a61419ec552aa09607842
Bluthman2 <bluthman2@gmail.com>
2016-02-12 19:37:16 -0500

    test

--- ic2_lcd.js
+++ ic2_lcd.js
@@ -6,5 +6,9 @@ lcd = new LCDPLATE(1, 0x27);
 console.log("Working on lcd!");
 lcd.clear();
 
-lcd.message('Hello World!');
-
+lcd.createChar(1, [0,0,10,31,31,14,4,0]);
+lcd.createChar(2, [0,4,10,17,17,10,4,0]);
+ 
+lcd.clear();
+lcd.backlight(lcd.colors.RED);
+lcd.message('I\x01 n\x02de.js', true);
