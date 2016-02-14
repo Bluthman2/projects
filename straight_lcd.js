55b2263406754d8ef74f0ba2f273298e8ad81e7c
Bluthman2 <bluthman2@gmail.com>
2016-02-14 12:08:31 -0500

    go back to example

--- straight_lcd.js
+++ straight_lcd.js
@@ -1,17 +1,40 @@
+// var Lcd = require('lcd'),
+//   lcd = new Lcd({
+//     rs: 12,
+//     e: 21,
+//     data: [11, 13, 15, 16],
+//     cols: 8,
+//     rows: 2
+//   });
+ 
+// console.log("printing to lcd");
+// lcd.on('ready', function() {
+//   setInterval(function() {
+//     lcd.setCursor(0, 0);
+//     lcd.print("Hello world!");
+//   }, 1000);
+// });
+ 
+// // If ctrl+c is hit, free resources and exit.
+// process.on('SIGINT', function() {
+//   lcd.clear();
+//   lcd.close();
+//   process.exit();
+// });
+
 var Lcd = require('lcd'),
   lcd = new Lcd({
     rs: 12,
     e: 21,
-    data: [11, 13, 15, 16],
+    data: [5, 6, 17, 18],
     cols: 8,
     rows: 2
   });
  
-console.log("printing to lcd");
 lcd.on('ready', function() {
   setInterval(function() {
     lcd.setCursor(0, 0);
-    lcd.print("Hello world!");
+    lcd.print(new Date().toString().substring(16, 24));
   }, 1000);
 });
  
