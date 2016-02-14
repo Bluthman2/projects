bdbb23f666cd6e9e45f858d127e655a40c708318
Bluthman2 <bluthman2@gmail.com>
2016-02-14 11:53:16 -0500

    test

--- straight_lcd.js
+++ straight_lcd.js
@@ -3,17 +3,17 @@ var Lcd = require('lcd'),
     rs: 12,
     e: 21,
     data: [11, 13, 15, 16],
-    cols: 16,
+    cols: 8,
     rows: 2
   });
  
 console.log("printing to lcd");
 lcd.on('ready', function() {
-  lcd.print("Hello world!");
-  // setInterval(function() {
-  //   lcd.setCursor(0, 0);
-  //   lcd.print(new Date().toString().substring(16, 24));
-  // }, 1000);
+  // lcd.print("Hello world!");
+  setInterval(function() {
+    lcd.setCursor(0, 0);
+    lcd.print(new Date().toString().substring(16, 24));
+  }, 1000);
 });
  
 // If ctrl+c is hit, free resources and exit.
